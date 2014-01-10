5d680cd7926835172994605711db2989a8f7276b
Mort Yao <mort.yao@gmail.com>
2014-01-10 23:18:52 +0100

    finish Route Entity

--- src/frs/beans/Route.java
+++ src/frs/beans/Route.java
@@ -4,6 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * @author Mort Yao <mort.yao@gmail.com>
+ * @version 0.1
+ * @since  2014-01-10
+ *
+ * Route Entity.
+ */
 @Entity
 public class Route {
 
@@ -28,62 +35,62 @@ public class Route {
         this.price = price;
     }
 
-    // Getter for id
+    // Getter for id.
     public long getId() {
         return id;
     }
 
-    // Setter for id
+    // Setter for id.
     public void setId(long id) {
         this.id = id;
     }
 
-    // Getter for fromCode
+    // Getter for fromCode.
     public String getFromCode() {
         return fromCode;
     }
 
-    // Setter for fromCode
+    // Setter for fromCode.
     public void setFromCode(String fromCode) {
         this.fromCode = fromCode;
     }
 
-    // Getter for toCode
+    // Getter for toCode.
     public String getToCode() {
         return toCode;
     }
 
-    // Setter for toCode
+    // Setter for toCode.
     public void setToCode(String toCode) {
         this.toCode = toCode;
     }
 
-    // Getter for airlineCode
+    // Getter for airlineCode.
     public String getAirlineCode() {
         return airlineCode;
     }
 
-    // Setter for airlineCode
+    // Setter for airlineCode.
     public void setAirlineCode(String airlineCode) {
         this.airlineCode = airlineCode;
     }
 
-    // Getter for flightNo
+    // Getter for flightNo.
     public String getFlightNo() {
         return flightNo;
     }
 
-    // Setter for flightNo
+    // Setter for flightNo.
     public void setFlightNo(String flightNo) {
         this.flightNo = flightNo;
     }
 
-    // Getter for price
+    // Getter for price.
     public double getPrice() {
         return price;
     }
 
-    // Setter for price
+    // Setter for price.
     public void setPrice(double price) {
         this.price = price;
     }
