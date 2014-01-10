65eb72a982284bc182aac7bd552d3dc207fb28f8
Mort Yao <mort.yao@gmail.com>
2014-01-10 23:14:56 +0100

    finish Airline Entity

--- src/frs/beans/Airline.java
+++ src/frs/beans/Airline.java
@@ -4,6 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * @author Mort Yao <mort.yao@gmail.com>
+ * @version 0.1
+ * @since  2014-01-10
+ *
+ * Airline Entity.
+ */
 @Entity
 public class Airline {
 
@@ -22,32 +29,32 @@ public class Airline {
         this.airlineName = airlineName;
     }
 
-    // Getter for id
+    // Getter for id.
     public long getId() {
         return id;
     }
 
-    // Setter for id
+    // Setter for id.
     public void setId(long id) {
         this.id = id;
     }
 
-    // Getter for airlineCode
+    // Getter for airlineCode.
     public String getAirlineCode() {
         return airlineCode;
     }
 
-    // Setter for airlineCode
+    // Setter for airlineCode.
     public void setAirlineCode(String airlineCode) {
         this.airlineCode = airlineCode;
     }
 
-    // Getter for airlineName
+    // Getter for airlineName.
     public String getAirlineName() {
         return airlineName;
     }
 
-    // Setter for airlineName
+    // Setter for airlineName.
     public void setAirlineName(String airlinename) {
         this.airlineName = airlineName;
     }
