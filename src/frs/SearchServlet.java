dace9a271e196f0af5453b169579c4132c0f94f0
Mort Yao <mort.yao@gmail.com>
2014-01-11 02:39:21 +0100

    update Search Servlet

--- src/frs/SearchServlet.java
+++ src/frs/SearchServlet.java
@@ -1,21 +1,25 @@
 package frs;
 
-import frs.beans.Search;
+import frs.beans.*;
 
-import java.util.List; //
-import frs.beans.*; //
-
+import java.util.List;
+import java.util.Properties;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.naming.InitialContext;
+import javax.naming.Context;
+import javax.rmi.PortableRemoteObject;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
 
-import java.util.Properties;
-import javax.naming.InitialContext;
-import javax.naming.Context;
-import javax.rmi.PortableRemoteObject;
-
+/**
+ * @author Mort Yao <mort.yao@gmail.com>
+ * @version 0.1
+ * @since  2014-01-11
+ *
+ * Search Servlet.
+ */
 @WebServlet("/search")
 public class SearchServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -23,36 +27,46 @@ public class SearchServlet extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
+        // Call response.jsp
         this.getServletContext().getRequestDispatcher("/response.jsp").include(request, response);
-        out.println("<h1>" + request.getParameter("query") + "</h1>");
-        out.println("<br/><b>You are from: " + request.getParameter("from"));
-        out.println(", to: " + request.getParameter("to") + "</b>");
 
-        try {
-            Properties props = new Properties();
-            props.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.client.LocalInitialContextFactory");
-            Context ctx = new InitialContext(props);
+        if (request.getParameter("query").equals("search")) {
+            // Search
+            try {
+                String orig = request.getParameter("from");
+                String dest = request.getParameter("to");
 
-            Search search = (Search)ctx.lookup("SearchBeanRemote");
+                Properties props = new Properties();
+                props.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.client.LocalInitialContextFactory");
+                props.put("frsDatabase", "new://Resource?type=DataSource");
+                props.put("frsDatabase.JdbcDriver", "org.hsqldb.jdbcDriver");
+                props.put("frsDatabase.JdbcUrl", "jdbc:hsqldb:mem:frs_db");
+                Context context = new InitialContext(props);
 
-            String result = search.getData();
-            out.println("<br/><b>" + result + "</b>");
+                Search search = (Search)context.lookup("java:global/frs/Search");
 
-            final Properties p = new Properties();
-            p.put("airportDatabase", "new://Resource?type=DataSource");
-            p.put("airportDatabase.JdbcDriver", "org.hsqldb.jdbcDriver");
-            p.put("airportDatabase.JdbcUrl", "jdbc:hsqldb:mem:airport_db");
-            final Context context = new InitialContext(p);
-            Airports airports = (Airports)context.lookup("java:global/frs/Airports");
-            airports.addAirport(new Airport("GKA", "Goroka", "Goroka", -6.081689, 145.391881, 5282));
-            List<Airport> list = airports.getAirports();
-            out.println("<br/><h2>" + list.size() + "</h2>");
-            //out.println("<br/><h2>" + list.get(0).getCity() + "</h2>");
-            //for (Airport airport : list)
-            //    airports.deleteAirport(airport);
+                // Search for all possible routes
+                List<Route> routes = search.getRoutes(orig, dest);
 
-        } catch (Exception e) {
-            e.printStackTrace(out);
+                // Print all routes
+                if (routes.size() == 0) {
+                    out.println("<br/><b>Travel from " + orig);
+                    out.println("to " + dest);
+                    out.println("is not available.</b>");
+                } else {
+                    out.println("<br/><b>Travel from " + orig);
+                    out.println("to " + dest);
+                    out.println("is available:</b>");
+                    out.println("<br/>");
+                    for (Route route : routes) {
+                        out.println("[" + route.getAirlineCode() + "] :");
+                        out.println(route.getFromCode() + " -> " + route.getToCode());
+                        out.println("<br/>");
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace(out);
+            }
         }
     }
 }
