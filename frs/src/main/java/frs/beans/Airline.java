524bec19f5df1faf599c316be99d16cd9f4d8b3a
Mort Yao <mort.yao@gmail.com>
2014-01-14 22:45:40 +0100

    update Airline Entity

--- frs/src/main/java/frs/beans/Airline.java
+++ frs/src/main/java/frs/beans/Airline.java
@@ -3,15 +3,19 @@ package frs.beans;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 /**
  * @author Mort Yao <mort.yao@gmail.com>
- * @version 0.1
- * @since  2014-01-10
+ * @version 1.0
+ * @since  2014-01-14
  *
  * Airline Entity.
  */
 @Entity
+@NamedQueries({
+        @NamedQuery(name = "Airline.getAll", query = "SELECT a FROM Airline a")})
 public class Airline {
 
     @Id
